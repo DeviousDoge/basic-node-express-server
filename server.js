2d0fe01c5fc97e70f383ebaeb186bf726a18743d
DeviousDoge <pdillon0@gmail.com>
2023-03-08 09:33:35 -0500

    reformats comments

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require('path');
 // Define Variables 
 // Defining express as a top-level function to be re-used each time a request is made or the port is being listened to
 const app = express();
-// A numerical identifier that defines where a network starts and ends
-// This line of code sets the PORT variable to either the value of the PORT environment variable, if it is set, or 3001 if it is not set. 
-// This is useful when deploying the application to a hosting platform, as the hosting platform may specify the port that the application should listen on through an environment variable.
 
+
+// Sets the PORT variable to either the value of the PORT environment variable, if it is set, or 3001 if it is not set. 
+// This is useful when deploying the application to a hosting platform, as the hosting platform may specify the port that the application should listen on through an environment variable.
 const PORT = process.env.PORT || 3001;
 
 // Initialize server
